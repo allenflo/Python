cd63da56f9cc2ce0aa9c76f3870a6642b0b4a8f7
allenflo <allenflo@outlook.com>
2022-10-27 23:23:28 -0700

    Training

--- 100_Days_of_Code/Day16/main.py
+++ 100_Days_of_Code/Day16/main.py
@@ -1,9 +1,17 @@
 from turtle import Turtle, Screen
+from replit import clear
+
+clear()
+
 
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("purple")
+timmy.forward(100)
 
 my_screen = Screen()
 print(my_screen.canvheight)
+my_screen.exitonclick()
 
 
