3fdedf7b0a0fd47cd912cef7b8ab215fc0d4146f
allenflo <allenflo@outlook.com>
2022-11-02 23:15:11 -0700

    Turtle_Shapes

---
+++ 100_Days_of_Code/Day18/day-18-start.py
@@ -0,0 +1,30 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+
+
+for i in range(0, 5):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
