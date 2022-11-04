eb677cf70d23fb866901027ac43dd3d59422b2bb
allenflo <allenflo@outlook.com>
2022-11-04 16:43:42 -0700

    Snake Game

--- 100_Days_of_Code/Days_1-20/Day20_SnakeGame/main.py
+++ 100_Days_of_Code/Days_1-20/Day20_SnakeGame/main.py
@@ -1,1 +1,31 @@
-#import Turtle Graphics, Screen, Random
+# import Turtle Graphics, Screen, Random
+from turtle import Turtle, Screen
+from snake import Snake
+import time
+
+# import random
+
+# setup screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Allen's Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# Main game loop
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(.1)
+
+    snake.move()
+
+screen.exitonclick()
---
+++ 100_Days_of_Code/Days_1-20/Day20_SnakeGame/snake.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    # set starting positions and build segments
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        # Moving last segment to position of 2nd object and 2nd to 1st
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
+
