1766d20afbe69df33f5515d27a6506f183f8ec4a
allenflo <allenflo@outlook.com>
2022-11-06 11:23:14 -0700

    Check for duplicates

---
+++ 100_Days_of_Code/Day_21_Snake2/inheritance.py
@@ -0,0 +1,17 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes=2
+
+    def breath(self):
+        print("Inhale, exhale")
+
+class Fish:
+
+    def swim(self):
+        print("moving in water")
+
+nemo = Fish()
+nemo.swim()
+
+
+
